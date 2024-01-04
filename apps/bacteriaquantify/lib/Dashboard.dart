43bf399ebd83b5a3e5475fc89da89ded6c4582e9
araneta <aldo@aldoapp.com>
2024-01-04 12:23:47 +0700

    dashboard top pannel

--- apps/bacteriaquantify/lib/Dashboard.dart
+++ apps/bacteriaquantify/lib/Dashboard.dart
@@ -1,9 +1,11 @@
 import 'package:bacteriaquantify/services/UserService.dart';
 import 'package:bacteriaquantify/style.dart';
+import 'package:bacteriaquantify/utils/UserPreferences.dart';
 import 'package:bacteriaquantify/widgets/BigRoundIconButton.dart';
 import 'package:flutter/material.dart';
 
 import 'auth_screen.dart';
+import 'models/User.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
@@ -20,78 +22,118 @@ class _DashboardState extends State<Dashboard> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Image.asset(
-            'assets/bg.png',
-            width: double.infinity,
-            height: double.infinity,
-            fit: BoxFit.cover,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Bacteria\nQuantify!',
-                    style: TextStyle(
-                        color: Color(0XFF4FA6CB),
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold),
-                  )
-                ],
-              ),
-              const SizedBox(height: 54),
+    var size = MediaQuery.of(context).size;
+    User user = UserPreferences.getUser();
+
+    return SafeArea(
+        child: Scaffold(
+      body: CustomScrollView(slivers: [
+        SliverFillRemaining(
+            hasScrollBody: false,
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
               Container(
-                margin: const EdgeInsets.symmetric(horizontal: 24),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 28, vertical: 13),
-                child: Column(
-                  children: [
-                    Container(
-                        padding: EdgeInsets.only(bottom: 20.0, top: 4),
-                        width: 190,
-                        child: BigRoundIconButton(
-                            onTap: () async {
-                              print("take photo");
-                            },
-                            icon: AssetImage("add_a_photo_24px.png"),
-                            title: "Take a Photo")),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                        padding: EdgeInsets.only(bottom: 20.0, top: 4),
-                        width: 190,
-                        child: BigRoundIconButton(
-                            onTap: () async {
-                              print("insert photo");
-                            },
-                            icon: AssetImage("add_photo_alternate_24px.png"),
-                            title: "Insert Photo")),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                        padding: EdgeInsets.only(bottom: 20.0, top: 4),
-                        width: 190,
-                        child: BigRoundIconButton(
-                            onTap: () async {
-                              print("History photo");
-                            },
-                            icon: AssetImage("history_24px.png"),
-                            title: "History")),
-                  ],
+                width: double.infinity,
+                height: 90,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('top-pattern.png'),
+                    fit: BoxFit.cover,
+                  ),
                 ),
+                child: Container(
+                    padding: EdgeInsets.only(left: 40.0, top: 20, bottom: 10),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Selamat Datang!",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                              )),
+                          SizedBox(
+                            height: 6,
+                          ),
+                          Text(user.fullName,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700)),
+                        ])),
               ),
-            ],
-          )
-        ],
-      ),
-    );
+              Container(
+                  padding: EdgeInsets.only(top: 80, bottom: 60.0),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/bg.png'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'Bacteria\nQuantify!',
+                              style: TextStyle(
+                                  color: Color(0XFF4FA6CB),
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                        const SizedBox(height: 44),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 24),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 28, vertical: 13),
+                          child: Column(
+                            children: [
+                              Container(
+                                  padding:
+                                      EdgeInsets.only(bottom: 20.0, top: 4),
+                                  width: 190,
+                                  child: BigRoundIconButton(
+                                      onTap: () async {
+                                        print("take photo");
+                                      },
+                                      icon: AssetImage("add_a_photo_24px.png"),
+                                      title: "Take a Photo")),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Container(
+                                  padding:
+                                      EdgeInsets.only(bottom: 20.0, top: 4),
+                                  width: 190,
+                                  child: BigRoundIconButton(
+                                      onTap: () async {
+                                        print("insert photo");
+                                      },
+                                      icon: AssetImage(
+                                          "add_photo_alternate_24px.png"),
+                                      title: "Insert Photo")),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Container(
+                                  padding:
+                                      EdgeInsets.only(bottom: 20.0, top: 4),
+                                  width: 190,
+                                  child: BigRoundIconButton(
+                                      onTap: () async {
+                                        print("History photo");
+                                      },
+                                      icon: AssetImage("history_24px.png"),
+                                      title: "History")),
+                            ],
+                          ),
+                        ),
+                      ])),
+            ]))
+      ]),
+    ));
   }
 }
